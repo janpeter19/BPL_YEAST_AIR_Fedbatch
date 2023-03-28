9d0b34dfebdae6563b57f66048d893eabd65d08e
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2023-03-28 16:50:17 +0200

    Update to FMU-explore 0.9.7

--- BPL_YEAST_AIR_Fedbatch_DOcontrol_fmpy_explore.py
+++ BPL_YEAST_AIR_Fedbatch_DOcontrol_fmpy_explore.py
@@ -68,9 +68,9 @@ elif platform.system() == 'Linux':
 
 # Provide various opts-profiles
 if flag_type in ['CS', 'cs']:
-   opts_std = {'NCP': 500}
+   opts_std = {'ncp': 500}
 elif flag_type in ['ME', 'me']:
-   opts_std = {'NCP': 500}
+   opts_std = {'ncp': 500}
 else:    
    print('There is no FMU for this platform')
   
@@ -622,7 +622,7 @@ def simu(simulationTime=simulationTime, mode='Initial', options=opts_std, diagra
          validate = False,
          start_time = 0,
          stop_time = simulationTime,
-         output_interval = simulationTime/options['NCP'],
+         output_interval = simulationTime/options['ncp'],
          record_events = True,
          start_values = start_values,
          fmi_call_logger = None,
@@ -658,7 +658,7 @@ def simu(simulationTime=simulationTime, mode='Initial', options=opts_std, diagra
             validate = False,
             start_time = prevFinalTime,
             stop_time = prevFinalTime + simulationTime,
-            output_interval = simulationTime/options['NCP'],
+            output_interval = simulationTime/options['ncp'],
             record_events = True,
             start_values = start_values,
             fmi_call_logger = None,
