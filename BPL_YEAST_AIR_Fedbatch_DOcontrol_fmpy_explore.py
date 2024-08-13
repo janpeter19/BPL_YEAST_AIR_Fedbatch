35a70895560ecd7920fb7b4b1062f453d222fd46
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2024-08-13 11:01:47 +0200

    Update BPL_YEAST_AIR_Fedbatch_DOcontrol_fmpy_explore.py

--- BPL_YEAST_AIR_Fedbatch_DOcontrol_fmpy_explore.py
+++ BPL_YEAST_AIR_Fedbatch_DOcontrol_fmpy_explore.py
@@ -22,6 +22,7 @@
 # 2024-05-20 - Updated the OpenModelica version to 1.23.0-dev
 # 2024-06-01 - Corrected model_get() to handle string values as well - improvement very small and keep ver 1.0.0
 # 2024-07-03 - Updated with adjustments of names (connections to FixValue) due to new FMU with BPL 2.2.1 
+# 2024-08-13 - Corrected model_get() to handle calculatedParameters - call it ver 1.0.1
 #------------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------------------------------------
@@ -473,7 +474,7 @@ def describe(name, decimals=3):
 
 #------------------------------------------------------------------------------------------------------------------
 #  General code 
-FMU_explore = 'FMU-explore for FMPy version 1.0.0'
+FMU_explore = 'FMU-explore for FMPy version 1.0.1'
 #------------------------------------------------------------------------------------------------------------------
 
 # Define function par() for parameter update
@@ -513,24 +514,25 @@ def model_get(parLoc, model_description=model_description):
    for k in range(len(par_var)):
       if par_var[k].name == parLoc:
          try:
-            if par_var[k].name in start_values.keys():
-                  value = start_values[par_var[k].name]
-            elif par_var[k].variability in ['constant', 'fixed']: 
-               if par_var[k].type in ['Integer', 'Real']: 
-                  value = float(par_var[k].start)      
-               if par_var[k].type in ['String']: 
-                  value = par_var[k].start                        
+            if (par_var[k].causality in ['local']) & (par_var[k].variability in ['constant']):
+               value = float(par_var[k].start)                 
+            elif par_var[k].causality in ['parameter']: 
+               value = float(par_var[k].start)  
+            elif par_var[k].causality in ['calculatedParameter']: 
+               value = float(sim_res[par_var[k].name][0]) 
+            elif par_var[k].name in start_values.keys():
+               value = start_values[par_var[k].name]   
             elif par_var[k].variability == 'continuous':
                try:
                   timeSeries = sim_res[par_var[k].name]
-                  value = timeSeries[-1]
+                  value = float(timeSeries[-1])
                except (AttributeError, ValueError):
                   value = None
                   print('Variable not logged')
             else:
                value = None
          except NameError:
-            print('Error: Information available after first simution')
+            print('Error: Information available after first simulation')
             value = None          
    return value
    
