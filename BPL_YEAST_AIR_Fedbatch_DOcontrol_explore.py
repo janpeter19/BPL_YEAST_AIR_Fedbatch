f21381283596337d5b9db7ad4f8200a7890d4739
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2025-07-19 16:13:14 +0200

    Run new compliled Windows FMU with PyFMI 2.17.4
    I now used BPL 2.3.1 and I also use a setup-file that for JM FMU do not include Td and  N in the parDict and parLocation, but for OM FMU they are included.

--- BPL_YEAST_AIR_Fedbatch_DOcontrol_explore.py
+++ BPL_YEAST_AIR_Fedbatch_DOcontrol_explore.py
@@ -71,6 +71,7 @@
 # 2025-02-14 - Change from qO2lim to qO2max and use KsO2 and also introduce VO2_start and VCO2_start
 # 2025-06-28 - Updated for BPL 2.3.1 Linux MSL 4.1.0 and bring back parameters Td and D_start
 # 2025-07-02 - Change PIreg to PIDreg
+# 2025-07-19 - Include Td and N only for OM generated FMUs
 #------------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------------------------------------
@@ -206,9 +207,9 @@ parDict['DO_sensor_x_start'] = 87.0
 #parDict['samplePeriod'] = 0.1
 parDict['K'] = 10.0
 parDict['Ti'] = 0.5
-parDict['Td'] = 0.0
+if flag_vendor in ['OM', 'om']: parDict['Td'] = 0.0
 parDict['I_start'] = 0
-parDict['D_start'] = 0
+if flag_vendor in ['OM', 'om']: parDict['D_start'] = 0.0
 parDict['N_low'] = 500
 parDict['N_high'] = 2000
 
@@ -248,9 +249,9 @@ parLocation['DO_sensor_x_start'] = 'DOsensor.x_start'
 #parLocation['samplePeriod'] = 'PIDreg.samplePeriod'
 parLocation['K'] = 'PIDreg.K'
 parLocation['Ti'] = 'PIDreg.Ti'
-parLocation['Td'] = 'PIDreg.Td'
+if flag_vendor in ['OM', 'om']: parLocation['Td'] = 'PIDreg.Td'
 parLocation['I_start'] = 'PIDreg.I_start'
-parLocation['D_start'] = 'PIDreg.D_start'
+if flag_vendor in ['OM', 'om']: parLocation['D_start'] = 'PIDreg.D_start'
 parLocation['N_low'] = 'N_low.value'
 parLocation['N_high'] = 'N_high.value'
 
