0f4f869bfc8f178c340bec358b3804867ea9ee72
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2026-09-22 08:37:29 +0200

    Run in Colab with fmpy and FMU_explore 1.2.4 release

--- BPL_YEAST_AIR_Fedbatch_DOcontrol_fmpy.py
+++ BPL_YEAST_AIR_Fedbatch_DOcontrol_fmpy.py
@@ -4,11 +4,12 @@
 # 2026-09-08 - Created
 # 2026-09-09 - Drop global prevFinalTime and let it be just interal to fmu_explore_fmpy
 # 2026-09-18 - Decrease the framework to what is necessary and move matlotlib to the other setup-file
+# 2026-09-22 - Change indentaiton from 3 spaces to 4
 #------------------------------------------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 #  Framework
-#------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
 # Setup framework
 import platform
@@ -19,56 +20,56 @@ from fmpy import read_model_description
 # Set the environment - for Linux a JSON-file in the FMU is read
 if platform.system() == 'Linux': locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-#------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 #  Setup application FMU
-#------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
 # Provde the right FMU and load for different platforms in user dialogue:
 if platform.system() == 'Windows':
-   print('Windows - run FMU pre-compiled JModelica 2.14')
-   fmu_model ='BPL_YEAST_AIR_Fedbatch_DOcontrol_windows_jm_cs.fmu'        
-   model_description = read_model_description(fmu_model)  
-   flag_vendor = 'JM'
-   flag_type = 'CS'
+    print('Windows - run FMU pre-compiled JModelica 2.14')
+    fmu_model ='BPL_YEAST_AIR_Fedbatch_DOcontrol_windows_jm_cs.fmu'        
+    model_description = read_model_description(fmu_model)  
+    flag_vendor = 'JM'
+    flag_type = 'CS'
 elif platform.system() == 'Linux':  
-   flag_vendor = 'OM'
-   flag_type = 'ME'
-   if flag_vendor in ['OM','om']:
-      print('Linux - run FMU pre-compiled OpenModelica') 
-      if flag_type in ['CS','cs']:         
-         fmu_model ='BPL_YEAST_AIR_Fedbatch_DOcontrol_om_cs.fmu'    
-         model_description = read_model_description(fmu_model) 
-      if flag_type in ['ME','me']:         
-         fmu_model ='BPL_YEAST_AIR_Fedbatch_DOcontrol_linux_om_me.fmu'    
-         model_description = read_model_description(fmu_model) 
-   else:    
-      print('There is no FMU for this platform')
+    flag_vendor = 'OM'
+    flag_type = 'ME'
+    if flag_vendor in ['OM','om']:
+        print('Linux - run FMU pre-compiled OpenModelica') 
+        if flag_type in ['CS','cs']:         
+            fmu_model ='BPL_YEAST_AIR_Fedbatch_DOcontrol_om_cs.fmu'    
+            model_description = read_model_description(fmu_model) 
+        if flag_type in ['ME','me']:         
+            fmu_model ='BPL_YEAST_AIR_Fedbatch_DOcontrol_linux_om_me.fmu'    
+            model_description = read_model_description(fmu_model) 
+    else:    
+        print('There is no FMU for this platform')
 
 # Provide various opts-profiles
 if flag_type in ['CS', 'cs']:
-   opts_std = {'NCP': 500}
+    opts_std = {'NCP': 500}
 elif flag_type in ['ME', 'me']:
-   opts_std = {'NCP': 500}
+    opts_std = {'NCP': 500}
 else:    
-   print('There is no FMU for this platform')
+    print('There is no FMU for this platform')
   
 # Provide various MSL and BPL versions
 if flag_vendor in ['JM', 'jm']:
-   constants = [v for v in model_description.modelVariables if v.causality == 'local'] 
-   MSL_usage = [x[1] for x in [(constants[k].name, constants[k].start) \
-                     for k in range(len(constants))] if 'MSL.usage' in x[0]][0]   
-   MSL_version = [x[1] for x in [(constants[k].name, constants[k].start) \
-                       for k in range(len(constants))] if 'MSL.version' in x[0]][0]
-   BPL_version = [x[1] for x in [(constants[k].name, constants[k].start) \
-                       for k in range(len(constants))] if 'BPL.version' in x[0]][0] 
+    constants = [v for v in model_description.modelVariables if v.causality == 'local'] 
+    MSL_usage = [x[1] for x in [(constants[k].name, constants[k].start) \
+                      for k in range(len(constants))] if 'MSL.usage' in x[0]][0]   
+    MSL_version = [x[1] for x in [(constants[k].name, constants[k].start) \
+                        for k in range(len(constants))] if 'MSL.version' in x[0]][0]
+    BPL_version = [x[1] for x in [(constants[k].name, constants[k].start) \
+                        for k in range(len(constants))] if 'BPL.version' in x[0]][0] 
 elif flag_vendor in ['OM', 'om']:
-   MSL_usage = '4.1.0 - used components: RealInput, RealOutput, LimPID-components' 
-   MSL_version = '4.1.0'
-   BPL_version = 'Bioprocess Library version 2.3.2' 
+    MSL_usage = '4.1.0 - used components: RealInput, RealOutput, LimPID-components' 
+    MSL_version = '4.1.0'
+    BPL_version = 'Bioprocess Library version 2.3.2' 
 else:    
-   print('There is no FMU for this platform')
+    print('There is no FMU for this platform')
    
-#------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
 # Simulation time
 simulationTime = 20.0
@@ -82,9 +83,9 @@ component_list_minimum = ['bioreactor', 'bioreactor.culture', 'bioreactor.gas_li
 # Provide process diagram on disk
 fmu_process_diagram ='BPL_YEAST_AIR_Fedbatch_DOcontrol_process_diagram_om.png'
 
-#------------------------------------------------------------------------------------------------------------------
-#  Specific application constructs: stateValue, parValue, parLocation, parCheck, diagrams, ax
-#------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#  Specific application constructs: parValue, parLocation, parCheck, diagrams, ax
+#--------------------------------------------------------------------------------------------------
 
 # Create dictionaries parValue[] and parLocation[]
 parValue = {}
